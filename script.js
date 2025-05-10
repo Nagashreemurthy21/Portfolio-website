2823eab3cf294cc46b3d4b9354021d81497c38a3
Nagashreemurthy21 <naagashree42@gmail.com>
2025-05-09 20:51:05 -1200

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+// Typed.js
+var typed = new Typed("#element", {
+    strings: ["Frontend Developer.", "Web Designer.", "JavaScript Enthusiast."],
+    typeSpeed: 80,
+    backSpeed: 50,
+    loop: true
+  });
+  
+  // tsParticles
+  tsParticles.load("tsparticles", {
+    particles: {
+      number: { value: 60 },
+      size: { value: 3 },
+      move: { enable: true, speed: 0.8 },
+      color: { value: "#d2aaff" },
+      links: {
+        enable: true,
+        distance: 120,
+        color: "#d2aaff",
+        opacity: 0.5
+      }
+    },
+    background: {
+      color: {
+        value: "#000014"
+      }
+    }
+  });
+  
